cd698917115894b169ed04241cf46805c9e487a7
Hsoren31 <sorensenhaley@gmail.com>
2026-01-05 13:57:53 -0700

    FEAT: validate user guesses through backend api
    Users guesses will be submited to the wheres waldo api to check if it
    was correct or not. If the guess was correct a marker will be placed and
    if not an alert will tell them it's not right.

--- src/components/CharacterModal/CharacterModal.jsx
+++ src/components/CharacterModal/CharacterModal.jsx
@@ -1,8 +1,13 @@
 import "./CharacterModal.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function CharacterModal({ modal, setModal, toggleMenu, mouseCoordinates }) {
   const dialogRef = useRef();
+  const [markers, setMarkers] = useState([]);
+  const [character, setCharacter] = useState("Waldo");
+  const guess = mouseCoordinates;
+  const stage1url =
+    "http://localhost:5433/96533e1a-d0b0-46ec-ab56-5c8157b0c2ac";
 
   const handleOutsideClick = (e) => {
     if (e.target === dialogRef.current) {
@@ -10,6 +15,35 @@ function CharacterModal({ modal, setModal, toggleMenu, mouseCoordinates }) {
       dialogRef.current.close();
     }
   };
+
+  async function handleSubmit() {
+    try {
+      let response = await fetch(stage1url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "Application/JSON",
+        },
+        body: JSON.stringify({ character, guess }),
+      });
+      let { result } = await response.json();
+      if (result) {
+        const newMarker = {
+          id: new Date().getTime(),
+          x: mouseCoordinates.x,
+          y: mouseCoordinates.y,
+        };
+        setMarkers([...markers, newMarker]);
+        setModal(false);
+        dialogRef.current.close();
+        return;
+      }
+      alert(result);
+      setModal(false);
+      dialogRef.current.close();
+    } catch (error) {
+      alert(error);
+    }
+  }
 
   useEffect(() => {
     if (modal) {
@@ -21,6 +55,16 @@ function CharacterModal({ modal, setModal, toggleMenu, mouseCoordinates }) {
 
   return (
     <>
+      {markers.map((marker) => (
+        <i
+          key={marker.id}
+          className="fa-regular fa-circle-check marker"
+          style={{
+            left: marker.x,
+            top: marker.y,
+          }}
+        ></i>
+      ))}
       {modal && (
         <div
           className="img_target"
@@ -40,13 +84,18 @@ function CharacterModal({ modal, setModal, toggleMenu, mouseCoordinates }) {
         }}
       >
         <button onClick={toggleMenu}>Close</button>
-        <form action="#">
-          <select name="characters" id="character-select">
-            <option value="waldo">Waldo</option>
-            <option value="wenda">Wenda</option>
-            <option value="wizard">Wizard Whitebeard</option>
-            <option value="odlaw">Odlaw</option>
-            <option value="woof">Woof</option>
+        <form action={handleSubmit}>
+          <select
+            name="characters"
+            id="character-select"
+            value={character}
+            onChange={(e) => setCharacter(e.target.value)}
+          >
+            <option value="Waldo">Waldo</option>
+            <option value="Wenda">Wenda</option>
+            <option value="Wizard">Wizard Whitebeard</option>
+            <option value="Odlaw">Odlaw</option>
+            <option value="Woof">Woof</option>
           </select>
           <button type="submit">Check</button>
         </form>
