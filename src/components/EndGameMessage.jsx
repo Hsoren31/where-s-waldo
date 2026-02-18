6f3add8ddcd9b90b5e11741fb4c88bef50802865
Hsoren31 <sorensenhaley@gmail.com>
2026-02-18 09:52:10 -0700

    FEAT: add form to submit player name/time end game

--- src/components/EndGameMessage.jsx
+++ src/components/EndGameMessage.jsx
@@ -1,7 +1,26 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
 
 function EndGameMessage({ gameOver, time }) {
   const modalRef = useRef();
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetch("http://localhost:5433/game/leaderboard", {
+      method: "Post",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: name }),
+    }).then(navigate("/leaderboard"));
+  }
 
   useEffect(() => {
     if (gameOver) {
@@ -13,9 +32,20 @@ function EndGameMessage({ gameOver, time }) {
     <>
       {gameOver && (
         <dialog ref={modalRef}>
-          <h1>Where's Waldo Space Station</h1>
-          <h1>WoHoo!!!</h1>
-          <p>{time}</p>
+          <h1>Space Station</h1>
+          <h2>Congratulations!!</h2>
+          <p>You found everyone in {time}</p>
+          <form>
+            <label htmlFor="name">Add your score to the Leaderboard</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={handleChange}
+            />
+            <button onClick={handleSubmit}>Enter</button>
+          </form>
         </dialog>
       )}
     </>
