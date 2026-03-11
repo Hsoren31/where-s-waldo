cd8acea6a84b669ff719552e7a98f4332391a355
Hsoren31 <sorensenhaley@gmail.com>
2026-03-11 12:18:25 -0600

    REFACTOR: update fetch links to backend api

--- src/hooks/fetchGuess.js
+++ src/hooks/fetchGuess.js
@@ -1,7 +1,7 @@
 async function fetchGame() {
   try {
     const response = await fetch(
-      `http://localhost:5433/game/create/3fbd9d38-cb29-492d-99de-8d4153caf34e`,
+      `https://wheres-waldo-api-production-a65d.up.railway.app/game/create/1f229e49-059c-4836-9e87-474b324e7659`,
       {
         method: "POST",
         credentials: "include",
@@ -10,7 +10,7 @@ async function fetchGame() {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
-    const result = response.json();
+    const result = await response.json();
     return result;
   } catch (error) {
     console.error(error);
@@ -19,14 +19,17 @@ async function fetchGame() {
 
 async function fetchGuess(guess) {
   try {
-    const response = await fetch("http://localhost:5433/game/guess", {
-      method: "Post",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(guess),
-    });
+    const response = await fetch(
+      "https://wheres-waldo-api-production-a65d.up.railway.app/game/guess",
+      {
+        method: "Post",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(guess),
+      }
+    );
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
@@ -40,7 +43,7 @@ async function fetchGuess(guess) {
 async function fetchLeaderboard() {
   try {
     const response = await fetch(
-      `http://localhost:5433/game/3fbd9d38-cb29-492d-99de-8d4153caf34e`,
+      `https://wheres-waldo-api-production-a65d.up.railway.app/game/1f229e49-059c-4836-9e87-474b324e7659`,
       {
         method: "Get",
       }
