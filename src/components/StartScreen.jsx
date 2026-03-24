b7a2aeadb6f6c72ff49d4fd045acaa742279e4d3
Hsoren31 <sorensenhaley@gmail.com>
2026-03-24 15:26:07 -0600

    FEAT: add how to play instructions to start screen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 function StartScreen({ startScreen, startGame }) {
   const dialogRef = useRef();
+  const [HowToPlayVisibility, setHowToPlayVisibility] = useState(false);
 
   useEffect(() => {
     if (startScreen) {
@@ -10,11 +11,39 @@ function StartScreen({ startScreen, startGame }) {
     }
   }, [startScreen]);
 
+  function showHowTo() {
+    setHowToPlayVisibility(true);
+  }
+
+  function closeHowTo() {
+    setHowToPlayVisibility(false);
+  }
+
   return (
     <dialog ref={dialogRef}>
       <h1>Space Station</h1>
+      <button onClick={showHowTo}>How to Play</button>
+      <HowToPlay visibility={HowToPlayVisibility} close={closeHowTo} />
       <button onClick={startGame}>Start</button>
     </dialog>
+  );
+}
+
+function HowToPlay({ visibility, close }) {
+  return (
+    <>
+      {visibility && (
+        <div>
+          <p>
+            Your goal is to find Waldo and his friends as quickly as you can!
+            Click anywhere on the image and select the character you found. Keep
+            going until you've found everyone!
+          </p>
+          <p>Note: Woof the dog is obscured and only his tail is showing.</p>
+          <button onClick={close}>Close</button>
+        </div>
+      )}
+    </>
   );
 }
 
