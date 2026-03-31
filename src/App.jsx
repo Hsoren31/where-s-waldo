7b7ea45588fe91ddb9fbd7e28d335c5c4761d2c7
Hsoren31 <sorensenhaley@gmail.com>
2026-03-31 14:47:00 -0600

    FEAT: fetch all stages from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
 import { Link } from "react-router";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [stages, setStages] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://wheres-waldo-api-production-a65d.up.railway.app/game/stages")
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
+      .then((response) => setStages(response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>A network error has occured.</p>;
+  }
+
   return (
     <>
       <header>
         <h1>Where's Waldo</h1>
       </header>
       <div id="stages">
-        <Card
-          image={"./space_station_wheres_waldo.jpg"}
-          title={"Space Station"}
-        />
+        <ul>
+          {stages &&
+            stages.map((stage) => (
+              <Card
+                key={stage.id}
+                image={stage.image.url}
+                title={stage.title}
+              />
+            ))}
+        </ul>
       </div>
     </>
   );
@@ -20,7 +51,7 @@ function App() {
 function Card({ image, title }) {
   return (
     <div id="stage_card">
-      <Link to="/game">
+      <Link to={`/${title}`}>
         <img src={image} />
         <h2>{title}</h2>
       </Link>
